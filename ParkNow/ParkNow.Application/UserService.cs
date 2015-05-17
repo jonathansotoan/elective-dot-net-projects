b0619769172fb6f80f85a4483b31a811fcbc7c9f
jonathansotoan <jonathansoto.an@gmail.com>
2015-05-17 12:24:25 -0500

    Added some untracked files

---
+++ ParkNow/ParkNow.Application/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ParkNow.DataAccess;
+
+namespace ParkNow.Application
+{
+    public class UserService
+    {
+        private readonly Repository<User> _usersRepository;
+        private User _loggedUser;
+
+        public UserService(Repository<User> usersRepository = null)
+        {
+            _usersRepository = usersRepository ?? new ParkNowRepo<User>();
+        }
+
+        public User GetLoggedUser()
+        {
+            return _loggedUser;
+        }
+
+        public void SetLoggedUser(User loggedUser)
+        {
+            _loggedUser = loggedUser;
+        }
+
+        public bool DoesUserExist(string username, string password)
+        {
+            return _usersRepository.Get().Any(user => user.Username == username && user.Password == password);
+        }
+    }
+}
---
+++ ParkNow/ParkNow.DataAccess/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace ParkNow.DataAccess
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> Get()
+        {
+            return _dbSet;
+        }
+
+        public TEntity GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public TEntity Insert(TEntity entity)
+        {
+            return _dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            TEntity entityToDelete = _dbSet.Find(id);
+
+            if (entityToDelete != null)
+            {
+                Delete(entityToDelete);
+            }
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            _dbSet.Remove(entityToDelete);
+        }
+    }
+}
